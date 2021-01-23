589b00082fb605cec60b0bb156ef409f5d32ee2a
Lakshminarasimhan Parthasarathy <laky@ymail.com>
2021-01-22 22:56:49 -0800

    Update get_version.sh

--- get_version.sh
+++ get_version.sh
@@ -1,33 +1,30 @@
 #!/bin/sh
 set -e
-if [[ -z $1 || -z $2 ]]; then
-  echo "usage: $(basename $0) [Mmp] major.minor.patch"
-  exit 1
-fi
-if [[ "$1" -ne "M" && "$1" -ne "m" && "$1" -ne "p" ]]; then
-  echo "usage: $(basename $0) [Mmp] major.minor.patch"
+
+if [[ "$1" -ne "major" && "$1" -ne "minor" && "$1" -ne "patch" ]]; then
+  echo "usage: $(basename $0) [major/minor/patch] major.minor.patch"
   exit 1
 fi
 version=$2
 ver=( ${version//./ } )
 if [ ${#ver[@]} -ne 3 ]
 then
-  echo "usage: $(basename $0) [Mmp] major.minor.patch"
+  echo "usage: $(basename $0) [major/minor/patch] major.minor.patch"
   exit 1
 fi
-if [ "$1" == "M" ]
+if [ "$1" == "major" ]
 then
   ((ver[0]++))
   ver[1]=0
   ver[2]=0
 fi
-if [ "$1" == "m" ]
+if [ "$1" == "minor" ]
 then
   ((ver[1]++))
   ver[2]=0
 fi
-if [ "$1" == "p" ]
+if [ "$1" == "patch" ]
 then
   ((ver[2]++))
 fi
-export NEW_VERSION="${ver[0]}.${ver[1]}.${ver[2]}"
+echo "${ver[0]}.${ver[1]}.${ver[2]}"
